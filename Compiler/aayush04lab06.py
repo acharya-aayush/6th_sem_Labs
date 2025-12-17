069f3cf598a7b216079736a652ad8897c6782f27
Its me Aayush <acharyaaayush2k4@gmail.com>
2025-12-17 06:38:44 +0545

    Compiler lab 6

--- Compiler/aayush04lab06.py
+++ Compiler/aayush04lab06.py
@@ -1,24 +1,31 @@
+
 # Grammar: S→Aa | b ; A→c
-G = {'S': [['A','a'], ['b']], 'A': [['c']]}
+G = {'S': [['A','a'], ['b']], 'A': [['c']]}  # Define the grammar as a dictionary
 
-def first(X, G, memo):
-    if X not in G: return {X}              # terminal
-    if X in memo:  return memo[X]
-    F = set()
-    for prod in G[X]:
-        for sym in prod:
-            f = first(sym, G, memo)
-            F |= (f - {'ε'})
-            if 'ε' not in f: break
-        else:
-            F.add('ε')
-    memo[X] = F
-    return F
 
-memo = {}
-FS, FA = first('S', G, memo), first('A', G, memo)
-print("FIRST(A) =", FA)
-print("FIRST(S) =", FS)
+def first(X, G, memo):  # Function to compute FIRST set for symbol X
+    if X not in G:  # If X is not a non-terminal (not in grammar keys)
+        return {X}  # It's a terminal, so FIRST(X) is itself
+    if X in memo:   # If FIRST(X) is already computed
+        return memo[X]  # Return the cached result
+    F = set()  # Initialize an empty set for FIRST(X)
+    for prod in G[X]:  # For each production of X
+        for sym in prod:  # For each symbol in the production (from left to right)
+            f = first(sym, G, memo)  # Recursively compute FIRST(sym)
+            F |= (f - {'ε'})  # Add all elements of FIRST(sym) except epsilon to F
+            if 'ε' not in f:  # If epsilon is not in FIRST(sym)
+                break  # Stop checking further symbols in this production
+        else:  # If we didn't break (all symbols can derive epsilon)
+            F.add('ε')  # Add epsilon to FIRST(X)
+    memo[X] = F  # Cache the computed FIRST(X)
+    return F  # Return the FIRST set for X
+
+
+memo = {}  # Dictionary to store computed FIRST sets
+FS = first('S', G, memo)  # Compute FIRST(S)
+FA = first('A', G, memo)  # Compute FIRST(A)
+print("FIRST(A) =", FA)  # Print FIRST(A)
+print("FIRST(S) =", FS)  # Print FIRST(S)
 # --------------------------------------------------
 # THEORY:
 # FIRST(X) is the set of terminals that can appear
